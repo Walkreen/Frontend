4a0f6a10294d46676b3bf4d851e52f83e826a396
YGwan <50222603+YGwan@users.noreply.github.com>
2022-05-17 14:53:56 +0900

    Mission 화면 tabbar 구현

--- lib/MainScreen/Mission/MissionPage.dart
+++ lib/MainScreen/Mission/MissionPage.dart
@@ -55,6 +55,7 @@ class _MissionPageState extends State<MissionPage> {
                     style: TextStyle(
                         fontSize: 18.0
                     ),),
+
                 ),
               ],
             ),
